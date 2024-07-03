7c6cb1051c24d1a5dfb049bef3df65098d0bdd12
Madan939 <madantamang939@gmail.com>
2024-07-03 13:37:23 +0545

    new update

--- Backend/controllers/usercontroller.js
+++ Backend/controllers/usercontroller.js
@@ -1,11 +1,41 @@
-const Usermodel = require('../models/userModel')
+const Usermodel = require('../models/userModel');
+const tokenModel=require('../models/tokenmodel');
+const sentMail=require('../middleware/sentMail');
+const crypto=require('crypto');
 exports.userRegister = async (req, res) => {
     try {
-        const newUser = new Usermodel({ ...req.body, verified: false })
-        await newUser.save()
+        console.log(req.headers.host)
+        let newUser = new Usermodel({ ...req.body, verified: false })
+        newUser =await newUser.save()
         //res.send("user registered successfully")
-        res.status(200).json({
-            message: 'registered succesfully, wait for verification'
+        if(!newUser){
+            return res.status(400).json({
+                err:"failed to create user "
+            })
+        }
+       
+        let token=new tokenModel({
+            token:crypto.randomBytes(16).toString('hex'),
+            userId:newUser._id
+        }) 
+        token=await token.save()
+        if(!token){
+            return res.status(400).json({
+                err:"failed to store token "
+            })
+        }
+        //send email
+        sentMail({
+            from:'no-reply@email.com',
+            to:newUser.email,
+            subject:"email verification link",
+            text:`hello \n\n please verify your email by click link below:\n\n
+            http:\/\/${req.headers.host}\/api\/confirmation\/${token.token}
+            `
+       // http://localhost:8001/api/confirmation/absd111 
+        })
+        return res.status(200).json({
+            message: 'registered succesfully, check email for verification'
         })
     }
     catch (err) {
